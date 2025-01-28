0ebf9aa8234a368fced096c4241d54669252d407
royalphp <royalphpofficial@gmail.com>
2025-01-28 17:53:03 +0200

    Upgrade Sylius and PayPal Plugin; introduce new entities
    Updated Sylius to 1.14 and PayPal Plugin to 1.7, replacing deprecated configurations and routes. Added new entities for AvatarImage, ShopBillingData, and various translations to extend functionality. Removed unused dependencies and adjusted security settings for the new API structure.

--- src/Entity/Promotion/CatalogPromotion.php
+++ src/Entity/Promotion/CatalogPromotion.php
@@ -6,9 +6,14 @@ namespace App\Entity\Promotion;
 
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\CatalogPromotion as BaseCatalogPromotion;
+use Sylius\Component\Promotion\Model\CatalogPromotionTranslationInterface;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_catalog_promotion')]
 class CatalogPromotion extends BaseCatalogPromotion
 {
+    protected function createTranslation(): CatalogPromotionTranslationInterface
+    {
+        return new CatalogPromotionTranslation();
+    }
 }
--- src/Entity/Promotion/Promotion.php
+++ src/Entity/Promotion/Promotion.php
@@ -6,9 +6,14 @@ namespace App\Entity\Promotion;
 
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Promotion as BasePromotion;
+use Sylius\Component\Promotion\Model\PromotionTranslationInterface;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_promotion')]
 class Promotion extends BasePromotion
 {
+    protected function createTranslation(): PromotionTranslationInterface
+    {
+        return new PromotionTranslation();
+    }
 }
