ba4903574900eff080444e61db456bb72a910e1a
royalphp <129624547+royalphp@users.noreply.github.com>
2025-02-10 02:12:29 +0200

    Refactor configurations and update dependencies (#8)
    Updated Symfony configurations and switched to the `http_discovery` library for PSR-17 factories. Modified PayPal integration logic to handle checkout state checks. Added cron job for coupon generation and upgraded dependencies, including Symfony, Doctrine, and PHPUnit versions.

--- src/Scheduler/DefaultScheduleProvider.php
+++ src/Scheduler/DefaultScheduleProvider.php
@@ -25,6 +25,7 @@ final readonly class DefaultScheduleProvider implements ScheduleProviderInterfac
             ->add(
                 RecurringMessage::cron('* * * * *', new RunCommandMessage('sylius:remove-expired-carts')),
                 RecurringMessage::cron('* * * * *', new RunCommandMessage('sylius:cancel-unpaid-orders')),
+                RecurringMessage::cron('* * * * *', new RunCommandMessage('sylius:promotion:generate-coupons')),
             )
             ->stateful($this->cache)
         ;
