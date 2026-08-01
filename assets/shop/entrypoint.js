03655308caabf73d90839eb1493d3c5a5efbb607
royalphp <129624547+royalphp@users.noreply.github.com>
2026-08-01 19:03:06 +0300

    Update dependencies, simplify setup, and integrate payment plugins (#32)
    * Update dependencies in `composer.lock` to the latest versions
    
    * Update `symfony.lock` to latest recipe reference
    
    * Update `symfony.lock` to latest Stimulus bundle recipe version
    
    * Update dependencies and version constraints in `package-lock.json` and `symfony.lock`
    
    Tightened Webpack version range, updated Symfony UX asset versions, and bumped recipe reference in `symfony.lock` to latest.
    
    * Remove redundant Dev Container configuration, unused scripts, and simplify project setup
    
    * Integrate Stripe, Adyen, and Refund plugins with updated routes, configurations, and dependencies.

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,2 +1,4 @@
 import '@vendor/sylius/mollie-plugin/assets/shop/entrypoint';
+import '@vendor/flux-se/sylius-stripe-plugin/assets/shop/entrypoint';
+import '@vendor/sylius/adyen-plugin/assets/shop/entrypoint';
 import './bootstrap.js';
